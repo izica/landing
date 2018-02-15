443e466ac80187f37db10a273c41c02be6f55112
izica <artemiztomska@gmail.com>
2018-02-16 02:10:41 +0700

    today parallax

--- src/js/module/today.js
+++ src/js/module/today.js
@@ -1,1 +1,16 @@
-console.log('123');
+jQuery(document).ready(function($) {
+    var calc = function(object){
+        var coef_y = object.attr('data-parallax-y');
+        var coef_window = $(window).scrollTop();
+        var y = coef_y * 40 + coef_window * 0.05 - 60;
+        object.css('transform', 'translate(0px, ' + y + 'px)');
+    }
+    $(".parallax").each(function(index, el) {
+        calc($(this));
+    });
+    $(window).on('scroll', function(event) {
+        $(".parallax").each(function(index, el) {
+            calc($(this));
+        });
+    });
+});
