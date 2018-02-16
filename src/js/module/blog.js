3c4e4c9759adf23d1b401788f67e0c8d3a3c87a3
izica <artemiztomska@gmail.com>
2018-02-16 16:22:51 +0700

    blog

---
+++ src/js/module/blog.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+    var mySwiper = new Swiper('.blog-slider', {
+        slidesPerView: 2,
+        speed: 400,
+        spaceBetween: 30,
+        centeredSlides: true,
+        loop: true
+    });
+});
